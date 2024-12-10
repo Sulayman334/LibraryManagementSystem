5266b36fd4d6336b71ec52b5e16f90ffc49eaf1a
Sulayman334 <sulaymancolley526@gmail.com>
2024-12-10 23:44:37 +0000

    Added the spring security dependency to be able to complete the role base functionality and login

---
+++ src/main/java/com/example/LibraryManagementSystem/configuration/SecurityConfiguration.java
@@ -0,0 +1,19 @@
+package com.example.LibraryManagementSystem.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration  {
+
+
+
+}
